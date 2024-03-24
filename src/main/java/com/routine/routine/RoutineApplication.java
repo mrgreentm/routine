7fd525bde8baebf3eb26ebb8b40ad41854ed3bfa
joao neto <joaonetotfm@gmail.com>
2024-03-23 23:57:37 -0300

    todo

--- src/main/java/com/routine/routine/RoutineApplication.java
+++ src/main/java/com/routine/routine/RoutineApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RoutineApplication {
-
+	// TODO : Utilizar Clean Architecture
 	public static void main(String[] args) {
 		SpringApplication.run(RoutineApplication.class, args);
 	}
