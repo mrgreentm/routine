58c0327f252c2b67b2915eef782909a86001355b
joao neto <joaonetotfm@gmail.com>
2024-03-23 23:56:44 -0300

    feature: adiciona logs

--- src/main/java/com/routine/routine/controllers/TaskController.java
+++ src/main/java/com/routine/routine/controllers/TaskController.java
@@ -6,34 +6,50 @@ import com.routine.routine.models.TaskRequestDTO;
 import com.routine.routine.models.TaskResponseDTO;
 import com.routine.routine.services.TaskService;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @AllArgsConstructor
 public class TaskController {
+    private final Logger logger = LoggerFactory.getLogger(TaskController.class);
+
     private final TaskService taskService;
 
     @GetMapping("/task")
-    public ResponseEntity<List<TaskEntity>> helloRoutine() {
-        return ResponseEntity.ok(taskService.getAllTasks());
+    public ResponseEntity<List<TaskEntity>> getAllTasks() {
+        logger.info("Capturar todas tasks inicializado");
+        List<TaskEntity> taskEntities = taskService.getAllTasks();
+        logger.info("Capturar todas tasks finalizado");
+        return ResponseEntity.ok(taskEntities);
     }
     @PostMapping("/task")
     public ResponseEntity<TaskResponseDTO> createTask(@RequestBody TaskRequestDTO task) {
-        return ResponseEntity.ok(taskService.createTask(task));
+        logger.info("Criar task inicializado");
+        TaskResponseDTO taskResponseDTO = taskService.createTask(task);
+        logger.info("Criar task finalizado");
+        return ResponseEntity.ok(taskResponseDTO);
     }
     @PutMapping("/task/{id}")
     public ResponseEntity<TaskEntity> changeStatusTask(
             @PathVariable Long id,
             @RequestBody ChangeTaskRequestDTO task
     ) {
-        return ResponseEntity.ok(taskService.changeStatusTask(id, task));
+        logger.info("Editar task inicializado");
+        TaskEntity taskEntity = taskService.changeStatusTask(id, task);
+        logger.info("Editar task finalizado");
+        return ResponseEntity.ok(taskEntity);
     }
     @DeleteMapping("/task/{id}")
     public void deleteTask(@PathVariable Long id) {
+        logger.info("Delete task inicializado");
         taskService.deleteTask(id);
+        logger.info("Delete task finalizado");
     }
 
 }
